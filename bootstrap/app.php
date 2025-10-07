f36bc1209af0a37c28337f84dc67c0a0f2ba7478
Andre Floquet <andrefloquet@gmail.com>
2025-10-07 23:03:31 +1000

    Other test to 419 error

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,6 +20,11 @@ return Application::configure(basePath: dirname(__DIR__))
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
         ]);
 
+        $middleware->validateCsrfTokens(except: [
+            'login', 
+            'register', 
+        ]);
+
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
