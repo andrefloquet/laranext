684c65486f6342e3834e229ff532b8fe6236dd16
Andre Floquet <andrefloquet@gmail.com>
2025-10-08 21:19:48 +1000

    create Post files
    runcommand 'php artisan make:model Post -a'

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://nextvel.vercel.app'],
+    'allowed_origins' => ['http://localhost:3000'],
 
     'allowed_origins_patterns' => [],
 
