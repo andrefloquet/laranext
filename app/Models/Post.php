eb9a90962ebde59fb36301443986610dc06af786
Andre Floquet <andrefloquet@gmail.com>
2025-10-08 23:36:29 +1000

    Post and User relationships

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,9 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+use App\Models\User;
 
 class Post extends Model
 {
     /** @use HasFactory<\Database\Factories\PostFactory> */
     use HasFactory;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }     
 }
